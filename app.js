d1af033ac4e3b77e291bf8220384ff53892b27dc
Anthony Short <antshort+github@gmail.com>
2013-09-07 16:21:05 -0700

    Added basic server

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var http = require('http');
+var hbs = require('hbs');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.engine('html', hbs.__express);
+app.use(app.router);
+
+app.get('/:project/:user', function(req, res){
+  res.render('index.html');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
