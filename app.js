1d3091b85b5ba6c7889fe1b6aa6c009a68211f68
Anthony Short <antshort+github@gmail.com>
2013-09-07 17:34:05 -0700

    Fixed path to build.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.engine('html', hbs.__express);
+app.use('/build', express.static(__dirname + '/build'));
 app.use(app.router);
 
 app.get('/:project/:user', function(req, res){
