12ae71f909896a68cff34e301386caa745e0855d
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-07 17:13:10 -0700

    added to router in browser boot

--- browser.js
+++ browser.js
@@ -11,19 +11,36 @@ var router = new Router();
 
 
 /**
+ * User route.
+ *
+ * @param {String} user
+ */
+
+router.on('/:user', function (context, next) {
+  // TODO
+});
+
+
+/**
  * Project route.
+ *
+ * @param {String} user
+ * @param {String} project
  */
 
 router.on('/:user/:project', function (context, next) {
   var user = context.params.user;
   var project = context.params.project;
-  if (!user || !project) throw new Error('no user & project');
+  if (!user || !project) throw new Error('no user or project');
 
-  var app = new Project({
-    user: user,
-    project: project
-  });
-
+  var app = new Project(user + '/' + project);
   document.body.innerHTML = '';
   document.body.appendChild(app.el);
 });
+
+
+/**
+ * Start the router.
+ */
+
+router.go();
